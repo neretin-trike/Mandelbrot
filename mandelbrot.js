9d785bfc2e961c4ba3b5fdb5cadef54746912a50
neretin-trike <hawktrike@gmail.com>
2017-10-17 21:55:44 +0700

    Добавил контекст холста

---
+++ mandelbrot.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    var canvas = document.getElementById("surface");
+    var context = canvas.getContext("2d");
+    //alert( 'Документ и все ресурсы загружены' );
+};
