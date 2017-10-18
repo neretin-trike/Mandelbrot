017b1ddd52aae669945260ccdc646f936494b876
Artem Neretin <hawktrike@gmail.com>
2017-10-18 10:18:57 +0700

    Добавил очистку холста

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,11 +1,13 @@
 var x = 0,
     y = 0,
     alfa = 0.3,
-    beta = 0.44083;
-        
+    beta = 0.44083
+    canvas = null,
+    context = null;
+
 window.onload = function() {
-    var canvas = document.getElementById("surface");
-    var context = canvas.getContext("2d");
+    canvas = document.getElementById("surface");
+    context = canvas.getContext("2d");
 
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -13,27 +15,35 @@ window.onload = function() {
     var figRatio = canvas.width/canvas.height,
         figScale = 750;
 
-    context.lineWidth = 2;
+    CanvasClear();
+    DrawMandelbrotSet(x,y,alfa,beta,figScale);    
+};
 
+function CanvasClear(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    DrawAxis();
+}
+
+function DrawAxis(){
+
+    context.shadowBlur = 0;
+    context.lineWidth = 2;
+    
+    context.beginPath();
     context.strokeStyle = "rgba(200, 230, 201, 0.15)";
     context.moveTo(canvas.width/2,0);
     context.lineTo(canvas.width/2,canvas.height);
     context.stroke();
 
     context.beginPath();
-
     context.strokeStyle = "rgba(200, 230, 201, 0.15)";
     context.moveTo(0,canvas.height/1.2);
     context.lineTo(canvas.width,canvas.height/1.2);
     context.stroke();
+}
 
 
-    DrawMandelbrotSet(x,y,alfa,beta,context,canvas,figScale);    
-
-};
-
-
-function DrawMandelbrotSet(x,y,alfa,beta,context,canvas,figScale){
+function DrawMandelbrotSet(x,y,alfa,beta,figScale){
 
     for (var i=0; i<580; i++){
         x_new = x*x-y*y+alfa;
@@ -46,8 +56,7 @@ function DrawMandelbrotSet(x,y,alfa,beta,context,canvas,figScale){
         context.arc(x_new*figScale+canvas.width/2,-y_new*figScale+canvas.height/1.2, 2, 0, Math.PI*2, true);
         context.fill();
 
-
-        console.log("После "+ x_new + " " + y_new);
+        // console.log("После "+ x_new + " " + y_new);
 
         context.shadowColor = 'blue';
         context.shadowBlur = 1;
