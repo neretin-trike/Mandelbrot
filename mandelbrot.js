cdf3b2e250a0d78058e0a3d5bde25060d70d29ce
Artem Neretin <hawktrike@gmail.com>
2017-10-18 10:51:54 +0700

    Добавил таймер и анимацию рисования

--- mandelbrot.js
+++ mandelbrot.js
@@ -3,7 +3,9 @@ var x = 0,
     alfa = 0.3,
     beta = 0.44083
     canvas = null,
-    context = null;
+    context = null,
+    myTimer = null,
+    figScale = 750;
 
 window.onload = function() {
     canvas = document.getElementById("surface");
@@ -12,11 +14,10 @@ window.onload = function() {
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    var figRatio = canvas.width/canvas.height,
-        figScale = 750;
+    var figRatio = canvas.width/canvas.height;
 
     CanvasClear();
-    DrawMandelbrotSet(x,y,alfa,beta,figScale);    
+    StartDrawMandelbrot();
 };
 
 function CanvasClear(){
@@ -28,7 +29,7 @@ function DrawAxis(){
 
     context.shadowBlur = 0;
     context.lineWidth = 2;
-    
+
     context.beginPath();
     context.strokeStyle = "rgba(200, 230, 201, 0.15)";
     context.moveTo(canvas.width/2,0);
@@ -42,15 +43,24 @@ function DrawAxis(){
     context.stroke();
 }
 
+var i=0;
+function StartDrawMandelbrot(){
+    i = 0;
+    myTimer = setInterval(
+                function (){
+                    DrawMandelbrotSet(x,y,alfa,beta,figScale)
+                }
+                ,1);
+}
 
 function DrawMandelbrotSet(x,y,alfa,beta,figScale){
 
-    for (var i=0; i<580; i++){
+    // for (var i=0; i<580; i++){
         x_new = x*x-y*y+alfa;
         y_new = 2*x*y+beta;
 
         context.shadowColor = 'red';
-        context.shadowBlur = 10;
+        context.shadowBlur = 5;
         context.beginPath();
         context.fillStyle = '#E91E63';
         context.arc(x_new*figScale+canvas.width/2,-y_new*figScale+canvas.height/1.2, 2, 0, Math.PI*2, true);
@@ -67,9 +77,15 @@ function DrawMandelbrotSet(x,y,alfa,beta,figScale){
         context.lineTo(x_new*figScale+canvas.width/2,-y_new*figScale+canvas.height/1.2);
         context.stroke();
 
-        x = x_new;
-        y = y_new;
-    }
+        this.x = x_new;
+        this.y = y_new;
+
+        // console.log(i);
+        i++;
+        if ( i==2500){
+            clearInterval(myTimer);
+        }
+    // }
 }
 
     // context.beginPath();
